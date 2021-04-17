2c12de8be2737c2f2c268a936d59b3e6ceb5f89a
omidBehnia <omidBehnia9@gmail.com>
2021-04-17 18:14:11 +0430

    set Email activation Code

--- app/src/main/java/com/example/omidmarket/AccountFragment.java
+++ app/src/main/java/com/example/omidmarket/AccountFragment.java
@@ -1,19 +1,144 @@
 package com.example.omidmarket;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.Random;
+
 public class AccountFragment extends Fragment {
 
+    private EditText email,ver1,ver2,ver3,ver4,ver5,ver6;
+    private Button sendPhone,sendVerify;
+    private LinearLayout sendLayout, verifyLayout;
+    private int randomNumber;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.acconut_fragment, container, false);
+        View view =inflater.inflate(R.layout.acconut_fragment, container, false);
+
+        email = view.findViewById(R.id.email);
+
+        sendPhone = view.findViewById(R.id.send_phone);
+        sendVerify = view.findViewById(R.id.send_verify);
+
+        sendLayout = view.findViewById(R.id.send_layout);
+        verifyLayout = view.findViewById(R.id.verify_layout);
+
+        ver1 = view.findViewById(R.id.ver1);
+        ver2 = view.findViewById(R.id.ver2);
+        ver3 = view.findViewById(R.id.ver3);
+        ver4 = view.findViewById(R.id.ver4);
+        ver5 = view.findViewById(R.id.ver5);
+        ver6 = view.findViewById(R.id.ver6);
+
+        show_sendLayout();
+        
+        sendPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = "omidbehnia9@gmail.com";
+                final String password = "Omid1371";
+                Random random = new Random();
+                randomNumber = random.nextInt(99999)+100000;
+                String messageToSend = "Your Code is : " + randomNumber;
+                Properties props = new Properties();
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.host","smtp.gmail.com");
+                props.put("mail.smtp.port","587");
+                Session session = Session.getInstance(props,
+                        new javax.mail.Authenticator(){
+                            @Override
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(username, password);
+                            }
+                        });
+                try{
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(username));
+                    message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(email.getText().toString()));
+                    message.setSubject("به امید مارکت خوش آمدید");
+                    message.setText(messageToSend);
+                    Transport.send(message);
+
+                    Toast.makeText(getContext(), "Email is Successfully", Toast.LENGTH_SHORT).show();
+                    show_verifyLayout();
+                }catch(MessagingException e){
+                    Toast.makeText(getContext(), "Error" + e.getMessage() , Toast.LENGTH_SHORT).show();
+                    Log.e("TAG", "onClick: " + e.getMessage() );
+                }
+            }
+        });
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        next_editText(ver1 , ver2);
+        next_editText(ver2 , ver3);
+        next_editText(ver3 , ver4);
+        next_editText(ver4 , ver5);
+        next_editText(ver5 , ver6);
+
+        sendVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        return view;
     }
+
+    public void show_sendLayout(){
+        sendLayout.setVisibility(View.VISIBLE);
+        verifyLayout.setVisibility(View.GONE);
+    }
+    public void show_verifyLayout(){
+        sendLayout.setVisibility(View.GONE);
+        verifyLayout.setVisibility(View.VISIBLE);
+    }
+
+
+
+    public void next_editText(EditText ver , EditText verFocus){
+
+        ver.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(ver.getText().toString().length() == 1){
+                    verFocus.requestFocus();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/omidmarket/MainActivity.java
+++ app/src/main/java/com/example/omidmarket/MainActivity.java
@@ -1,22 +1,44 @@
 package com.example.omidmarket;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottom_navigation;
     FrameLayout fragment_container;
+    Toolbar myToolbar;
+    EditText editText1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+        editText1 = findViewById(R.id.editText1);
+
+
+
+
         bottom_navigation = findViewById(R.id.bottom_navigation);
         fragment_container = findViewById(R.id.fragment_container);
 
@@ -47,6 +69,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
 }
